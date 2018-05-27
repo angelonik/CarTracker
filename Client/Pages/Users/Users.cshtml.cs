6266fda3bc05ec8869c69af2f5f2605325895bbb
angelo_nikoloulo <angelosnikoloulo@gmail.com>
2018-05-27 22:19:00 +0100

    split users to seperated component and created a loading component

---
+++ Client/Pages/Users/Users.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using DomainModel;
+using Microsoft.AspNetCore.Blazor.Components;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor;
+
+namespace Client.Pages.Users
+{
+    public class UsersModel : BlazorComponent
+    {
+        [Inject] protected HttpClient Http { get; set; }
+        protected User[] Users = Array.Empty<User>();
+        protected bool Loading;
+
+        protected override async Task OnInitAsync()
+        {
+            Loading = true;
+            Users = await Http.GetJsonAsync<User[]>("/api/users");
+            Loading = false;
+        }
+
+        protected async Task RefreshTable()
+        {
+            await OnInitAsync();
+            StateHasChanged();
+        }
+    }
+}
